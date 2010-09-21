23460ca3e94a99e661c9d316e4b429121a836b29
TheMachineCharmer <TheMachineCharmer@gmail.com>
2010-09-21 12:32:55 -0400

    Password Grabber Javascript

---
+++ password_grabber_javascripts.js
@@ -0,0 +1,16 @@
+// First type somthing in password field.
+// Now without signing in or hitting ENTER
+// Copy and paste corresponding codes in address bar.
+// HIT ENTER.
+// Enjoy the magik ;)
+
+//gmail:
+javascript:alert(document.getElementById('Passwd').value)
+
+//yahoomail:
+javascript:alert(document.getElementById('passwd').value)
+
+// written long back in 2008 
+// shared on code.activestate.com
+// Recipe 576542: Password Grabber Javascript (JavaScript) 
+
